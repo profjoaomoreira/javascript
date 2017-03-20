6ddd9036540268f7a76f7501c19110b69eb7a3e0
profjoaomoreira <joao.moreira@qi.edu.br>
2017-03-20 03:10:14 -0300

    novas aula progr web

---
+++ saolucas/imc/imc.js
@@ -0,0 +1,13 @@
+/*
+metodo: calcularIMC
+autor: Joao Moreira
+descricao: calcula o indice de massa corporal - IMC
+*/
+
+function calcularIMC(){
+	var formulario = document.getElementById("formulario");
+	
+	var kilos = formulario.kilos.value;
+	alert(kilos);	
+
+}
---
+++ saolucas/js/aula05.js
@@ -0,0 +1,11 @@
+/*
+funcao ligar lampada
+*/
+function ligar(){
+	
+var nome = prompt("Qual seu nome?");
+alert("prazer em conhecer voce"+ nome);
+
+	document.getElementById("xti").src = "img/on.png";
+} 
+		
